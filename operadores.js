f325f37e8c4f2d5d22242a17049b7f9105288ee1
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-26 08:36:33 -0300

    Insert Operadores

---
+++ operadores.js
@@ -0,0 +1,28 @@
+// Operadores
+
+//Operadores de atribuição   x = y, x = x (+,-,*,/,>>,<<,&,^,|) y
+
+
+//Operadores de comparação
+
+/* ==, !=, ====, !==, >, >= , < , <= */
+
+//Operadores aritméticos
+
+/* %, ++, --, -, +, **, */
+
+//Operadores bit a bit
+
+/* & , | , ^, ~, <<,  */
+
+/* Operadores lógicos */
+
+/* Operadores de string */
+
+/* Operador condicional (ternário) */
+
+/* Operador vírgula */
+
+/* Operadores unário */
+
+/* Operadores relacionais */
