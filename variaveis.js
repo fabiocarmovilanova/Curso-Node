660c54227d276ccbfbe87acd3beda3d896c5b706
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-26 08:24:39 -0300

    Exercicos variaveis globais locais constantes

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+/* variavel var e let variaveis de scopos, globais e escopos de cima e de dentro
+ não setadas */
+var NaoSetada;
+
+//setadas
+var setada = 10 
+
+
+//variavel constantes
+const valorPresetadoObrigatorio =  3.14;
+let string = "Alguma String"
+
